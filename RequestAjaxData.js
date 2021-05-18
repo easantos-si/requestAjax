b1687a80ce8a2de8d9984b25f34ce18e068fba70
easantos <45570763+easantos-si@users.noreply.github.com>
2021-05-18 11:58:37 -0300

    Create RequestAjaxData.js

---
+++ RequestAjaxData.js
@@ -0,0 +1,54 @@
+class RequestAjaxData {
+    data;
+    url;
+    type;
+    callbackActionSuccess;
+    callbackActionError;
+    callbackActionComplete;
+
+    constructor(url, type = 'POST', callbackSuccess = null, callbackError = null, callbackComplete = null) {
+        this.url = url;
+        this.type = type;
+        this.data = {}
+        this.callbackActionSuccess = callbackSuccess;
+        this.callbackActionError = callbackError;
+        this.callbackActionComplete = callbackComplete;
+    }
+
+    propertyAdd = (name, value) =>{
+        this.data[name] = value;
+    }
+
+    setData = (data) => {
+        this.data = data;
+    }
+
+    callback = (data) => {
+        if( data.success && this.callbackActionSuccess || !data.success && this.callbackActionError ){
+            if(data.success) {
+                this.callbackActionSuccess(data);
+            }
+            else{
+                this.callbackActionError(data)
+            }
+        }
+        else{
+            this.callbackDefault(data);
+        }
+    }
+
+    callbackDefault = (data) =>{
+        if(data.success){
+            window.location.reload();
+        }
+        else{
+
+        }
+    }
+
+    callbackComplete = () => {
+        if(this.callbackActionComplete){
+            this.callbackActionComplete();
+        }
+    }
+}
